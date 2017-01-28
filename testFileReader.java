40220e1a72b319cffa790e2f07a29da555a4505d
hchiam <hchiam@gmail.com>
2017-01-27 23:06:26 -0500

    loop through number of tokens for more reusability

--- testFileReader.java
+++ testFileReader.java
@@ -9,16 +9,17 @@ import java.util.StringTokenizer;
 
 public class testFileReader {
     public static void main(String[] args) throws Exception {
+        int numOfTokens = 3;
         try {
             FileReader file = new FileReader("testfile.txt");
             BufferedReader reader = new BufferedReader(file);
             String line = reader.readLine();
             while (line != null) {
                 StringTokenizer tokens = new StringTokenizer(line);
-                if (tokens.countTokens() == 3) {
-                    System.out.println(tokens.nextToken());
-                    System.out.println(tokens.nextToken());
-                    System.out.println(tokens.nextToken());
+                if (tokens.countTokens() == numOfTokens) {
+                    for (int i=1;i<=numOfTokens;i++) {
+                        System.out.println(tokens.nextToken());
+                    }
                 }
                 line = reader.readLine();
             }
