251129e037c8a835350caab18d37a690a03b18a5
hchiam <hchiam@gmail.com>
2017-01-31 21:49:05 -0500

    add example minimal library

---
+++ testLibrary/Main.java
@@ -0,0 +1,6 @@
+// based on http://www.programcreek.com/2011/07/build-a-java-library-for-yourself/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(SimpleLibrary.add(1, 2));
+	}
+}
---
+++ testLibrary/SimpleLibrary.java
@@ -0,0 +1,6 @@
+// based on http://www.programcreek.com/2011/07/build-a-java-library-for-yourself/
+public class SimpleLibrary {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
