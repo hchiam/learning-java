fd86fce71ea71f804bbb3f1014ee64996b4312ef
hchiam <hchiam@gmail.com>
2017-02-13 22:36:02 -0500

    format using google-java-format 1.3

--- testFileReader.java
+++ testFileReader.java
@@ -8,24 +8,24 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class testFileReader {
-    public static void main(String[] args) throws Exception {
-        int numOfTokens = 3;
-        try {
-            FileReader file = new FileReader("testfile.txt");
-            BufferedReader reader = new BufferedReader(file);
-            String line = reader.readLine();
-            while (line != null) {
-                StringTokenizer tokens = new StringTokenizer(line);
-                if (tokens.countTokens() == numOfTokens) {
-                    for (int i=1;i<=numOfTokens;i++) {
-                        System.out.println(tokens.nextToken());
-                    }
-                }
-                line = reader.readLine();
-            }
-        } catch (Exception e) {
-            System.err.println("Exception");
-            // do something
+  public static void main(String[] args) throws Exception {
+    int numOfTokens = 3;
+    try {
+      FileReader file = new FileReader("testfile.txt");
+      BufferedReader reader = new BufferedReader(file);
+      String line = reader.readLine();
+      while (line != null) {
+        StringTokenizer tokens = new StringTokenizer(line);
+        if (tokens.countTokens() == numOfTokens) {
+          for (int i = 1; i <= numOfTokens; i++) {
+            System.out.println(tokens.nextToken());
+          }
         }
+        line = reader.readLine();
+      }
+    } catch (Exception e) {
+      System.err.println("Exception");
+      // do something
     }
+  }
 }
