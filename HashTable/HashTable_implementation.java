8395b9a0f314f9920566e5c350968e1e569864f0
hchiam <hchiam@gmail.com>
2017-03-03 20:42:36 -0500

    backup

---
+++ HashTable/HashTable_implementation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class HashTable {
+    int size;
+    int fullnessRatio = 0;
+    String[] array; // later to: LinkedList[] array or ArrayList<LinkedList> array
+    public HashTable(int size) {
+        this.size = size;
+    }
+    public void add(String x) {
+        
+    }
+    public boolen alreadyHas(String x) {
+        
+    }
+    public void removeVal(String x) {
+        
+    }
+    public void rehash() {
+    }
+    public int hashFunction() {
+        // quadratic probing // later: separate chaining
+    }
+    public static void Main(String[] args) {
+        HashTable ht = new HashTable();
+    }
+}
